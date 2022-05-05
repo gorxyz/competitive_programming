6e40716faa099bc2d191473ad02dc5ddaf26aa61
sudurraaa <sudurrahah@protonmail.com>
2022-05-05 09:53:42 +0400

    init commit

---
+++ templates/d.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	return 0;
+}
